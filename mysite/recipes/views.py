ea68934397c9bedfaa5c2f7745b792197b0e3eb6
Isabella <iborkovic@berkeley.edu>
2021-06-10 22:51:45 -0700

    placeholder pages added

--- mysite/recipes/views.py
+++ mysite/recipes/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+
 
 # Create your views here.
+
+def index(request):
+	context = None
+	return render(request, 'recipes/index.html', context)
+
+def search(request):
+	context = None
+	return render(request, 'recipes/search.html', context)
+
+def results(request):
+	context = None
+	return render(request, 'recipes/results.html', context)
+
+def about(request):
+	context = None
+	return render(request, 'recipes/about.html', context)
+
+
