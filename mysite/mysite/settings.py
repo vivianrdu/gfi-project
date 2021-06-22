7b278442da0caa48b226f91e55b2ddfbf7cc7fe0
Vivian Du <vivian_du@berkeley.edu>
2021-06-22 15:49:17 -0700

    Modify database for django to integrate with postgresql

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from config import get_configs
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,11 +75,16 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+configs = get_configs()
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'gfi_project',
+        'USER': configs['username'],
+        'PASSWORD': configs['password'],
+        'HOST': configs['host'],
+        'PORT': configs['port']
     }
 }
 
