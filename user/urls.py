920efddef3b8fbfe8c0614cc7132ac1609bf03b2
vivianrdu <vivian_du@berkeley.edu>
2021-08-12 17:51:03 -0700

    Revamp file locations/setup for clarity, implement signup system with email authentication

---
+++ user/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('index.html', views.index),
+    path('about.html', views.about),
+    path('search.html', views.search),
+    path('results.html', views.results),
+    path('login.html', views.login),
+    path('signup.html', views.signup),
+    path('account_activation_email.html', views.signup),
+]
